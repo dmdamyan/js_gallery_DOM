5571d75d4d346fef76030f359d7d440a8bc938fe
Dmitrii Damyan <dmitriignc@gmail.com>
2024-12-24 20:35:33 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const nav = document.querySelector('.list-item');
+const imgLarge = document.querySelector('.gallery__large-img');
+const a = document.querySelector('a');
+const img = document.querySelector('.gallery__thumb');
+
+a.addEventListener('click', () => {
+  event.preventDefault();
+});
+
+img.addEventListener('click', () => {
+  event.preventDefault();
+});
+
+nav.addEventListener('click', () => {
+  const link = event.target.closest('.list-item__link', '.gallery__img');
+
+  if (!link) {
+    return;
+  }
+
+  imgLarge.src = link.src;
+});
